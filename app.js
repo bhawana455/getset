975e11de9f8c7db94c5cae6bbe48dda62b124c10
bhawanapriyadarshini <bhawanapriyadarshini00@gmail.com>
2020-07-30 11:53:00 +0530

    create

--- app.js
+++ app.js
@@ -12,15 +12,19 @@ const navSlide = () => {
            link.style.animation = '';
        }
        else{
-          link.style.animation = 'navLinkFade 0.5s ease forwards $(index /7 + 1.5)s';
+          link.style.animation = `navLinkFade 0.5s ease forwards ${index /7 + 1.5}s`;
 
        }
 
-
-
     });
+     burger.classList.toggle('toggle');
+
+
 
    });
+
+
+
 }
 navSlide();
    
